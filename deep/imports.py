aeceeeb5af2040ba7a43f15f8f32ab2f62c57a9b
msh2481 <msh2481@mail.ru>
2023-11-03 12:08:00 +0100

    Requirements

--- deep/imports.py
+++ deep/imports.py
@@ -9,7 +9,6 @@ from typing import Annotated, Any
 import einops as ein
 import lightning.pytorch as pl  # type: ignore
 import matplotlib.pyplot as plt
-import neptune  # type: ignore
 import numpy as np
 import pandas as pd
 import plotly.express as px  # type: ignore
@@ -21,10 +20,6 @@ from beartype import beartype as typed
 from beartype.door import die_if_unbearable as assert_type
 from beartype.typing import Callable, Iterable
 from beartype.vale import Is
-from datasets import load_dataset, load_from_disk  # type: ignore
 from jaxtyping import Float, Int
-from lightning.pytorch.callbacks import LearningRateMonitor  # type: ignore
-from lightning.pytorch.loggers import CSVLogger, NeptuneLogger  # type: ignore
 from numpy import ndarray as ND
 from torch import Tensor as TT
-from tqdm import tqdm
