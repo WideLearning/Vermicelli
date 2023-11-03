a7b786aa0e7003ae8a16881090bd0fe6a6745e4a
msh2481 <msh2481@mail.ru>
2023-11-03 12:03:48 +0100

    Test experiment

--- deep/train.py
+++ deep/train.py
@@ -3,6 +3,7 @@ from dvclive import Live  # type: ignore
 from dvclive.lightning import DVCLiveLogger  # type: ignore
 from data import PointDataset
 from models import ResMLP, BiResMLP, PointModel
+import yaml
 
 store = PointDataset(id_bits=8)
 store.load("img/vertical.png")
@@ -12,23 +13,26 @@ store.load("img/triangles.png")
 dl = t.utils.data.DataLoader(store, batch_size=64, shuffle=True, num_workers=4)
 val_dl = t.utils.data.DataLoader(store, batch_size=128, shuffle=False, num_workers=4)
 
+with open("params.yaml") as f:
+    params = yaml.safe_load(f)
+
 model = PointModel(
     store=store,
-    model_type="ResMLP",
+    model_type=params["model_type"],
     input_size=43,
-    hidden_size=64,
+    hidden_size=params["hidden_size"],
     output_size=3,
-    n_layers=10,
+    n_layers=params["n_layers"],
+    lr=params["lr"],
+    k=params["k"],
 )
+
 trainer = pl.Trainer(
     logger=DVCLiveLogger(
         log_model=True,
         # run_name="",
     ),
     check_val_every_n_epoch=5,
-    max_epochs=100,
+    max_epochs=params["max_epochs"],
 )
 trainer.fit(model, train_dataloaders=dl, val_dataloaders=val_dl)
-
-
-
